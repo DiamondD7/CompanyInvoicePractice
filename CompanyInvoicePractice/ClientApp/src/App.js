6da644e959f50dfa1a060b18da8467ad83911aef
diamond <aaron.diamond2@yahoo.co.nz>
2022-10-12 17:38:27 +1300

    added a register page with forms

--- CompanyInvoicePractice/ClientApp/src/App.js
+++ CompanyInvoicePractice/ClientApp/src/App.js
@@ -1,7 +1,8 @@
-﻿import React from 'react';
+﻿import React, {useState} from 'react';
 import Home from './components/Home';
 import BookTable from './components/BookTable';
 import CustomerTable from './components/CustomerTable';
+import Register from './components/RegisterForms/Register';
 
 export default function App() {
     let component;
@@ -15,6 +16,9 @@ export default function App() {
         case "/CustomerTable":
             component = <CustomerTable />
             break;
+        case "/Register":
+            component = <Register />
+            break;
         default:
             console.log("default error");
             break;
@@ -22,13 +26,19 @@ export default function App() {
 
     return (
         <div>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/BookTable">Books</a></li>
-                <li><a href="/CustomerTable">Members</a></li>
-            </ul>
+            <div className="d-flex justify-content-between">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/BookTable">Books</a></li>
+                    <li><a href="/CustomerTable">Members</a></li>
+                </ul>
+                <div className= "mt-3 me-3">
+                    <a className="btn btn-primary" href="/Register">Register</a>
+                    <a className="btn btn-outline-primary ms-3" href="/Register">Sign in</a>
+                </div>
+            </div>
 
             {component}
         </div>
-        )
+    )
 }
---
+++ CompanyInvoicePractice/ClientApp/src/components/RegisterForms/Register.jsx
@@ -0,0 +1,193 @@
+﻿import React, { useRef, useState, useEffect } from 'react';
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const Register = () => {
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+
+
+
+    const [user, setUser] = useState('');
+    const [validUser, setValidUser] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidUser(result);
+    }, [user])
+
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd));
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd, matchPwd])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg('Invalid Entry')
+            return;
+        }
+    }
+    return (
+        <section>
+            <form className="form" onSubmit={handleSubmit}>
+                <h1>Register</h1>
+
+                <label htmlFor="userfirstnameid" className="form--label">First name:</label>
+                <input className="form--input" id="userfirstnameid" type="text"
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+
+                <label htmlFor="userlastnameid" className="form--label">Last name:</label>
+                <input className="form--input" id="userlastnameid" type="text"
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+
+                <label htmlFor="userphoneid" className="form--label">Phone number:</label>
+                <input className="form--input" id="userphoneid" type="text"
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+
+                <label htmlFor="useremailid" className="form--label">Email:</label>
+                <input className="form--input" id="useremailid" type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <label htmlFor="username" className="form--label">
+                    Username:
+                    <span className={validUser ? "valid" : "hide"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="validation--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                    </span>
+
+                    <span className={validUser || !user ? "hide" : "invalid"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="validation--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </span>
+                </label>
+                <input className="form--input" id="username" type="text"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    required
+                    aria-invalid={validUser ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+
+                <p id="uidnote" className={userFocus && user && !validUser ? "instructions" : "offscreen"}>
+                    4 to 24 characters. <br />
+                    Must begin with a letter. <br />
+                    Letters, numbers, underscored, hyphens allowed.
+                </p>
+                
+
+                <label className="form--label">
+                    Password:
+                    <span className={validPwd ? "valid" : "hide"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="validation--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                    </span>
+
+                    <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="validation--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+
+                    </span>
+                </label>
+                <input className="form--input" type="password"
+                    ref={userRef}
+                    onChange={(e) => setPwd(e.target.value)}
+                    required
+                    aria-invalid={validUser ? "false" : "true"}
+                    aria-describedby="pwdnote"
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                />
+
+                <p id="pwdnote" className={pwdFocus && !validPwd ? "instructionsPwd" : "offscreen"}>
+                    8 to 24 characters. <br />
+                    Must include uppercase and lowercase letters, a number and a special character.<br />
+                    Allowed special characters: <span aria-label="exclamation mark">!</span>
+                    <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span>
+                    <span aria-label="dollar sign">$</span><span aria-label="percent">%</span>
+                </p>
+
+
+                <label className="form--label">
+                    Confirm Password:
+                    <span className={validPwd && validMatch && matchPwd ? "valid" : "hide"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="validation--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                    </span>
+
+                    <span className={validPwd || validMatch || !matchPwd ? "hide" : "invalid"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="validation--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+
+                    </span>
+                </label>
+                <input className="form--input" type="password"
+                    ref={userRef}
+                    id="confirm_pwd"
+                    onChange={(e) => setMatchPwd(e.target.value)}
+                    required
+                    aria-invalid={validMatch ? "false" : "true"}
+                    aria-describedby="confirmnote"
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p id="confirmnote" className={matchFocus && !validMatch ? "instructionsConfPwd" : "offscreen"}>
+                    Must match the first password input field
+                </p>
+
+
+                <button className="btn btn-primary mt-5" disabled={!validUser || !validPwd || !validMatch ? true : false}>Confirm</button>
+
+            </form>
+        </section>
+    )
+}
+
+export default Register;
